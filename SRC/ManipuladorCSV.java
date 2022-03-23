07bc1a47d818f04a23b3f89b622440427c29f2c3
Luisegc4 <luisegc@ciencias.unam.mx>
2022-03-23 00:17:41 -0600

    ManipuladorCsv

---
+++ SRC/ManipuladorCSV.java
@@ -0,0 +1,23 @@
+package SRC;
+
+import java.util.LinkedList;
+
+public class ManipuladorCSV {
+    /**
+     *
+     * @param nombrearchivocsv
+     * @return
+     */
+    public LinkedList<String> LeeCsv(String nombrearchivocsv){
+        return null;
+    }
+
+    /**
+     *
+     * @param lista
+     */
+    public void EscribeCsv(LinkedList<String> lista){
+
+    }
+
+}
