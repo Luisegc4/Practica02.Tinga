515b3c366cfe7061ad4043559804303be484eb84
Luisegc4 <luisegc@ciencias.unam.mx>
2022-03-22 23:07:57 -0600

    PersonayDireccionAtributos

---
+++ SRC/Direccion.java
@@ -0,0 +1,40 @@
+package SRC;
+
+public class Direccion {
+    String estado;
+    String calle;
+    String numero;
+    String CodigoPostal;
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCodigoPostal() {
+        return CodigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        CodigoPostal = codigoPostal;
+    }
+}
--- SRC/Persona.java
+++ SRC/Persona.java
@@ -2,5 +2,69 @@ package SRC;
 
 public class Persona {
     String id;
-    String Nombre;
+
+
+
+    String Nombres;
+
+
+
+    String ApellidoPaterno;
+    String ApellidoMaterno;
+    String CURP;
+    Direccion direccion;
+    String telefono;
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNombres() {
+        return Nombres;
+    }
+
+    public void setNombres(String nombres) {
+        Nombres = nombres;
+    }
+    public String getApellidoPaterno() {
+        return ApellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        ApellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return ApellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        ApellidoMaterno = apellidoMaterno;
+    }
+
+    public String getCURP() {
+        return CURP;
+    }
+
+    public void setCURP(String CURP) {
+        this.CURP = CURP;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
