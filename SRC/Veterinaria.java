ae91d899e988ce8f1a3911478b3afdb878f5c141
Luis Maro Escobar Rosales <53909978+luismarioescobarrosalesciencias@users.noreply.github.com>
2022-03-23 21:45:07 -0600

    Clase Veterinaria documentada

--- SRC/Veterinaria.java
+++ SRC/Veterinaria.java
@@ -1,5 +1,5 @@
 /**
- * Clase que modela una Mascota 
+ * Clase que modela una Veterinaria 
  * @author Luis Emilio Gonzalez Covarrubias
  * @author Luis Mario Escobar Rosales 
  * @author Melissa Vázquez González
@@ -68,42 +68,82 @@ public class Veterinaria {
         return apartado;
     }
 
+   /**
+    * Método getTelefono obtiene el telefono de la veterinaria 
+    * @return telefono -- regresa el telefono  de la veterinaria . 
+    */  
     public String getTelefono() {
         return telefono;
     }
-
+    
+   /**
+    * Método getHorario obtiene el horario de la veterinaria 
+    * @return horario -- regresa el horraio  de la veterinaria . 
+    */ 
     public Horario getHorario() {
         return horario;
     }
 
+   /**
+    * Método getDireccion obtiene la direccion de la veterinaria 
+    * @return direccion -- regresa la direccion de la veterinaria . 
+    */ 
     public Direccion getDireccion() {
         return direccion;
     }
-
+    
+   /**
+    * Método setNombres que define el nuevo nombre de la veterinaria. 
+    * @param  nombre -- El nuevo nombre de la veterinaria.
+    */
     public void setNombres(String nombre) {
         this.nombre = nombre;
     }
 
+   /**
+    * Método setTelefono que define el nuevo telefono de la veterinaria. 
+    * @param  telefono -- El nuevo telefono de la veterinaria.
+    */
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
+   /**
+    * Método setConsultorio que define el nuevo consultario de la veterinaria. 
+    * @param  consultorio -- El nuevo consultorio de la veterinaria.
+    */
     public void setConsultorio(String consultorio) {
         this.consultorios = consultorio;
     }
 
+    /**
+    * Método setApartado que define el nuevo apartado de la veterinaria. 
+    * @param  apartado -- El nuevo apartado de la veterinaria.
+    */
     public void setApartado(String apartado) {
         this.apartado = apartado;
     }
 
+   /**
+    * Método setHorario que define el nuevo horario de la veterinaria. 
+    * @param  horario -- El nuevo horario de la veterinaria.
+    */
     public void setHorario(Horario horario) {
         this.horario = horario;
     }
 
+    /**
+    * Método setDireccion que define la nueva direccion de la veterinaria. 
+    * @param  direccion --La nueva  direccion  de la veterinaria.
+    */
     public void setDireccion(Direccion direccion) {
         this.direccion = direccion;
     }
-
+    
+    /**
+    * Método toString que regresa un string que representa a la veterinaria  . 
+    * @return modelo de la veterinaria 
+    */
     @Override
     public String toString() {
         return nombre + "," +  consultorios + "," + apartado + "," + telefono + "," + horario.toString() +","+direccion.toString();
